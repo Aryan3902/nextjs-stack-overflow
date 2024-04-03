81ba192fce53161bc57a45fce9edf28a2533a550
Aryan <cse200001010@iiti.ac.in>
2024-04-03 23:48:21 +0530

    Hover class Minor fix

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -14,7 +14,7 @@ const HomeFilters = () => {
           ${
             active === filter.value
               ? "bg-primary-100 text-primary-500"
-              : "bg-light-800 text-light-500 hover:bg-light-800 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-300"
+              : "bg-light-800 text-light-500 hover:bg-light-700 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-400"
           }`}
         >
           {filter.name}
